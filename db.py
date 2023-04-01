d0b3e0d4a6cd89c202d3c07737e71d8b2b2d54e5
tumffa <tuomas.v.niemi@gmail.com>
2023-04-01 17:28:19 +0300

    Add pool_pre_ping = True when configuring db

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ from app import app
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}  
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 
 db = SQLAlchemy(app)
